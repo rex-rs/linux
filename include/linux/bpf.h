0aa3a5eda4b38f07ddf204660fa49e6595c4487c
Jinghao Jia <jinghao7@illinois.edu>
2024-06-11 23:46:21 -0500

    s/iu/rex/g
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -33,7 +33,7 @@
 #include <linux/cfi.h>
 #include <asm/rqspinlock.h>
 
-#include <asm/iu_unwind.h>
+#include <asm/rex_unwind.h>
 
 struct bpf_verifier_env;
 struct bpf_verifier_log;
@@ -1747,7 +1747,7 @@ struct bpf_prog_aux {
 	struct bpf_stream stream[2];
 };
 
-struct bpf_mem {
+struct rex_mem {
 	void *mem;
 	u32 total_page;
 };
@@ -1784,8 +1784,8 @@ struct bpf_prog {
 					    const struct bpf_insn *insn);
 	struct bpf_prog_aux	*aux;		/* Auxiliary fields */
 	struct sock_fprog_kern	*orig_prog;	/* Original BPF program */
-	struct bpf_mem		mem; /* inner-unikernel base program pages */
-	struct bpf_prog		*base; /* inner-unikernel base program */
+	struct rex_mem		mem;		/* Rex base program pages */
+	struct bpf_prog		*base;		/* Rex base program */
 	/* Instructions for interpreter */
 	union {
 		DECLARE_FLEX_ARRAY(struct sock_filter, insns);
--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -3371,7 +3371,7 @@ struct bpf_prog *bpf_prog_find_from_stack(void)
 
 #endif
 
-struct iu_cleanup_entry {
+struct rex_cleanup_entry {
 	u64 valid;
 	void *cleanup_fn;
 	void *cleanup_arg;
@@ -3379,4 +3379,4 @@ struct iu_cleanup_entry {
 
 #define IU_CLEANUP_ENTRIES_SIZE 64
 
-DEFINE_PER_CPU(struct iu_cleanup_entry[64], iu_cleanup_entries) ____cacheline_aligned = { 0 };
+DEFINE_PER_CPU(struct rex_cleanup_entry[64], rex_cleanup_entries) ____cacheline_aligned = { 0 };
