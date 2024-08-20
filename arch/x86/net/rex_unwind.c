4ef8500c4d793a1ea691847ed5ae83106263479b
Jinghao Jia <jinghao7@illinois.edu>
2024-08-19 23:40:10 -0500

    x86/rex_unwind: use unreachable() instead of BUG() to annotate noreturn
    This gets rid of the objtool warning complaining about unreachable
    instructions.
    
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- arch/x86/net/rex_unwind.c
+++ arch/x86/net/rex_unwind.c
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-#include <linux/bug.h>
 #include <linux/compiler_types.h>
 #include <linux/module.h>
 #include <linux/percpu.h>
@@ -82,5 +81,5 @@ __nocfi noinline void notrace __noreturn rex_landingpad(char *msg)
 	);
 
 	/* Unreachable, noreturn */
-	BUG();
+	unreachable();
 }
